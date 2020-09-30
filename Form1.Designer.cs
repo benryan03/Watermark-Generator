59d2314d06269e60ed229b16abe0b8863a42c98e
Ben Ryan <benryan03@gmail.com>
2020-09-30 15:54:45 -0400

    added ability to select multiple items in list

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -42,6 +42,7 @@
             this.listBox1.ItemHeight = 20;
             this.listBox1.Location = new System.Drawing.Point(12, 12);
             this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
             this.listBox1.Size = new System.Drawing.Size(776, 324);
             this.listBox1.TabIndex = 1;
             // 
