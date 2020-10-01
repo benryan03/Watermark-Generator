2d7705e3304ba269e83552cc7b23a56d91876eca
Ben Ryan <benryan03@gmail.com>
2020-10-01 18:47:33 -0400

    Layout updates

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,36 +29,32 @@
         private void InitializeComponent()
         {
             this.imageList = new System.Windows.Forms.ListBox();
-            this.browseButton = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.generateButton = new System.Windows.Forms.Button();
-            this.deleteButton = new System.Windows.Forms.Button();
-            this.watermarkTextBox = new System.Windows.Forms.TextBox();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.watermarkTextBox = new System.Windows.Forms.TextBox();
+            this.generateButton = new System.Windows.Forms.Button();
             this.statusStrip1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // imageList
             // 
             this.imageList.AllowDrop = true;
+            this.imageList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.imageList.FormattingEnabled = true;
             this.imageList.ItemHeight = 20;
-            this.imageList.Location = new System.Drawing.Point(12, 12);
+            this.imageList.Location = new System.Drawing.Point(0, 0);
             this.imageList.Name = "imageList";
             this.imageList.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
-            this.imageList.Size = new System.Drawing.Size(776, 324);
+            this.imageList.Size = new System.Drawing.Size(618, 344);
+            this.imageList.Sorted = true;
             this.imageList.TabIndex = 1;
-            // 
-            // browseButton
-            // 
-            this.browseButton.Location = new System.Drawing.Point(12, 342);
-            this.browseButton.Name = "browseButton";
-            this.browseButton.Size = new System.Drawing.Size(90, 40);
-            this.browseButton.TabIndex = 2;
-            this.browseButton.Text = "Browse";
-            this.browseButton.UseVisualStyleBackColor = true;
-            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
             // 
             // openFileDialog1
             // 
@@ -66,42 +62,14 @@
             this.openFileDialog1.Filter = "Images|*.jpg;*.jpeg;*.png;";
             this.openFileDialog1.Multiselect = true;
             // 
-            // generateButton
-            // 
-            this.generateButton.Location = new System.Drawing.Point(698, 342);
-            this.generateButton.Name = "generateButton";
-            this.generateButton.Size = new System.Drawing.Size(90, 40);
-            this.generateButton.TabIndex = 3;
-            this.generateButton.Text = "Generate";
-            this.generateButton.UseVisualStyleBackColor = true;
-            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
-            // 
-            // deleteButton
-            // 
-            this.deleteButton.Location = new System.Drawing.Point(108, 342);
-            this.deleteButton.Name = "deleteButton";
-            this.deleteButton.Size = new System.Drawing.Size(90, 40);
-            this.deleteButton.TabIndex = 4;
-            this.deleteButton.Text = "Delete";
-            this.deleteButton.UseVisualStyleBackColor = true;
-            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
-            // 
-            // watermarkTextBox
-            // 
-            this.watermarkTextBox.Location = new System.Drawing.Point(278, 349);
-            this.watermarkTextBox.Name = "watermarkTextBox";
-            this.watermarkTextBox.Size = new System.Drawing.Size(363, 26);
-            this.watermarkTextBox.TabIndex = 7;
-            this.watermarkTextBox.Text = "Watermark text";
-            // 
             // statusStrip1
             // 
             this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.statusLabel});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 418);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 392);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(800, 32);
+            this.statusStrip1.Size = new System.Drawing.Size(618, 32);
             this.statusStrip1.TabIndex = 0;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -111,21 +79,68 @@
             this.statusLabel.Size = new System.Drawing.Size(100, 25);
             this.statusLabel.Text = "statusLabel";
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.browseButton);
+            this.flowLayoutPanel1.Controls.Add(this.deleteButton);
+            this.flowLayoutPanel1.Controls.Add(this.watermarkTextBox);
+            this.flowLayoutPanel1.Controls.Add(this.generateButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 343);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(618, 49);
+            this.flowLayoutPanel1.TabIndex = 8;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(3, 3);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(90, 40);
+            this.browseButton.TabIndex = 3;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(99, 3);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(90, 40);
+            this.deleteButton.TabIndex = 5;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // watermarkTextBox
+            // 
+            this.watermarkTextBox.Location = new System.Drawing.Point(195, 3);
+            this.watermarkTextBox.Name = "watermarkTextBox";
+            this.watermarkTextBox.Size = new System.Drawing.Size(313, 26);
+            this.watermarkTextBox.TabIndex = 8;
+            this.watermarkTextBox.Text = "Watermark text";
+            // 
+            // generateButton
+            // 
+            this.generateButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.generateButton.Location = new System.Drawing.Point(514, 3);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(90, 40);
+            this.generateButton.TabIndex = 9;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(618, 424);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.watermarkTextBox);
-            this.Controls.Add(this.deleteButton);
-            this.Controls.Add(this.generateButton);
-            this.Controls.Add(this.browseButton);
             this.Controls.Add(this.imageList);
             this.Name = "Form1";
             this.Text = "Watermark Generator";
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,13 +148,14 @@
 
         #endregion
         private System.Windows.Forms.ListBox imageList;
-        private System.Windows.Forms.Button browseButton;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.Button generateButton;
-        private System.Windows.Forms.Button deleteButton;
-        private System.Windows.Forms.TextBox watermarkTextBox;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.TextBox watermarkTextBox;
+        private System.Windows.Forms.Button generateButton;
     }
 }
 
